615b3470bef657a0be8d7fa79b78c0db54927c0e
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-08-20 13:17:05 +0300

    add copy button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import template from './index.hbs';
-import scss from './index.scss';
-import juice from 'juice';
+import template from './index.hbs'
+import scss from './index.scss'
+import juice from 'juice'
 
-import bgImg from '../images/bg.png';
-import danaflexLogo from '../images/danaflex.png';
-import li1 from '../images/li/1.png';
-import li2 from '../images/li/2.png';
-import li3 from '../images/li/3.png';
-import li4 from '../images/li/4.png';
-import li5 from '../images/li/5.png';
+import bgImg from '../images/bg.png'
+import danaflexLogo from '../images/danaflex.png'
+import li1 from '../images/li/1.png'
+import li2 from '../images/li/2.png'
+import li3 from '../images/li/3.png'
+import li4 from '../images/li/4.png'
+import li5 from '../images/li/5.png'
 
 const neededVar = {bgImg, danaflexLogo, li1, li2, li3, li4, li5};
 
@@ -32,20 +32,27 @@ const data = {
     neededVar
 }
 
-
-
-document.getElementById('main-form').addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e);
+function getText () {
     const hbsResult = template({
         ...data,
         links: {
             ...data.links,
             signup: document.getElementById("signup").value
         }
-    })
-    const cssInlined = juice(hbsResult)
-    download('danaflex-email.html', cssInlined);
+    });
+    return juice(hbsResult);
+}
+
+
+
+document.getElementById('download').addEventListener("click", (e) => {
+    e.preventDefault();
+    download('danaflex-email.html', getText());
+})
+
+document.getElementById('copy').addEventListener("click", (e) => {
+    e.preventDefault();
+    copyTextToClipboard(getText());
 })
 
 function download(filename, text) {
@@ -65,4 +72,39 @@ if (window.location.href.indexOf('preview') > -1) {
     const hbsResult = template(data)
     const cssInlined = juice(hbsResult)
     document.documentElement.innerHTML = cssInlined
+}
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
 }
